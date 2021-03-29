fcdfe5e8c823d4a4c0738696ffe5cd180da769cf
박찬하 <chanhacraft@naver.com>
2021-03-29 10:15:33 +0900

    first commit

---
+++ src/main/java/kr/kro/gangseoopenchurch/GangseoopenchurchApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.gangseoopenchurch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangseoopenchurchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GangseoopenchurchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/kro/gangseoopenchurch/GangseoopenchurchApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.gangseoopenchurch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangseoopenchurchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
